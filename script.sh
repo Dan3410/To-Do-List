7fcd0d0b5be20dcdf79fe54a7bb4e5a57efde3f0
Tiger <danmats140@gmail.com>
2022-02-06 20:50:18 -0300

    Script modified to avoid using the command line interface

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Enter SQL's root password:"
-read rootPassword
+mysql_config_editor set --login-path=toDoList --host=localhost --user=root --password
 
-mysql -u root -p$rootPassword -e "create database toDoDB; CREATE USER 'toDoUser'@'localhost' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON toDoDB.* TO 'toDoUser'@'localhost'; FLUSH PRIVILEGES;"
+mysql --login-path=toDoList -e "create database toDoDB; CREATE USER 'toDoUser'@'localhost' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON toDoDB.* TO 'toDoUser'@'localhost'; FLUSH PRIVILEGES;"
 
 
 echo Installing necessary packages for BackEnd
